bcb8db69e3022dcc9e62a292422233dec3cea7a6
Yoshiki Osuka <207245001+Colorful-Wallet@users.noreply.github.com>
2025-09-04 05:33:56 +0900

    Initial Commit

---
+++ Navigation AppTests/Navigation_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_AppTests.swift
+//  Navigation AppTests
+//
+//  Created by Yoshiki Osuka on 2025/09/04.
+//
+
+import Testing
+@testable import Navigation_App
+
+struct Navigation_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
